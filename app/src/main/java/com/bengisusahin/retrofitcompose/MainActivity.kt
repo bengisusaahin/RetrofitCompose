6f2b73af74b342920b8e5d60076ef64733ba7eed
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-01-05 21:10:45 +0300

    lazy column

--- app/src/main/java/com/bengisusahin/retrofitcompose/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/retrofitcompose/MainActivity.kt
@@ -5,7 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import com.bengisusahin.retrofitcompose.ui.theme.ComposeRetroTheme
 
@@ -13,6 +21,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -80,11 +91,35 @@ fun AppBar(){
     }
 }
 
+@Composable
+fun CryptoList(cryptos:List<CryptoModel>){
+    LazyColumn(contentPadding = PaddingValues(5.dp)){
+        items(cryptos){ crypto->
+            CryptoRow(crypto = crypto)
+        }
+    }
+}
+
+@Composable
+fun CryptoRow(crypto:CryptoModel){
+    Column(modifier = Modifier.fillMaxWidth().background(color = MaterialTheme.colors.surface)) {
+        Text(text = crypto.currency,
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold
+            )
+        Text(text = crypto.price,
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier.padding(2.dp)
+            )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview(){
     ComposeRetroTheme{
-        MainScreen()
+        CryptoRow(crypto = CryptoModel("BTC","12321"))
     }
 
 }
