ecab8726f60d87b6c473ea36ad064961fb70f176
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-01-05 20:02:15 +0300

    topBar

--- app/src/main/java/com/bengisusahin/retrofitcompose/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/retrofitcompose/MainActivity.kt
@@ -1,23 +1,90 @@
 package com.bengisusahin.retrofitcompose
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.bengisusahin.retrofitcompose.ui.theme.ComposeRetroTheme
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bengisusahin.retrofitcompose.model.CryptoModel
+import com.bengisusahin.retrofitcompose.service.CryptoApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
-    private val BASE_URL = "https://raw.githubusercontent.com/"
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-
+            ComposeRetroTheme{
+                MainScreen()
+            }
         }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen(){
+    val BASE_URL = "https://raw.githubusercontent.com/"
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CryptoApi::class.java)
+
+    val call = retrofit.getData()
+
+    call.enqueue(object: Callback<List<CryptoModel>>{
+        override fun onResponse(
+            call: Call<List<CryptoModel>>,
+            response: Response<List<CryptoModel>>
+        ) {
+            if (response.isSuccessful){
+                response.body()?.let {
+                        //list
+
+                }
+            }
+        }
+
+        override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+            t.printStackTrace()
+        }
+
+    })
+
+    Scaffold(topBar = { AppBar() }) {
+
+    }
+
+}
+
+@Composable
+fun AppBar(){
+    androidx.compose.material.TopAppBar(contentPadding = PaddingValues(10.dp)){
+        Text(text = "Retrofit Compose", fontSize = 26.sp)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Greeting(){
+fun DefaultPreview(){
+    ComposeRetroTheme{
+        MainScreen()
+    }
 
 }
